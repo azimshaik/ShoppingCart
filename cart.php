008bc523eb37388b766d3a31c8ba5d6c4e89ca88
Azim Shaik <cheer.buddu@gmail.com>
2014-03-23 19:21:43 -0400

    cart
    cart test

--- cart.php
+++ cart.php
@@ -1,5 +1,21 @@
-<?php 
+<!--< ?php 
 session_start();
 echo $_SESSION["userid"];
 echo "cart"; 
+?>-->
+<h1>Cart</h1>
+<?php
+if(isset($_SESSION['cart'])){
+    $sql ="SELECT * FROM product WHERE id IN ("; 
+    foreach($_SESSION['cart']) as $id => $value){
+            $sql .= $id. ",";
+    }
+    $sql = subr($sql,0,-1) . ") ORDER BY ASC";
+}
+while($row = mysql_fetch_array($query)){
+?>
+    <p><"php echo $row['name']; ?><?php echo $_SESSION['cart'][$row['id']]['quantity'];?></p>
+<?php
+                                       }else{
+    echo "<p>Your cart is empty. Please add some products</p>";
 ?>
