fe6c9b295f67fb95311ff732533cdf2a88caf0b6
Azim Shaik <cheer.buddu@gmail.com>
2014-03-24 17:36:20 -0400

    cart code commit
    cart code issue

--- index.php
+++ index.php
@@ -1,12 +1,24 @@
 <?php
   session_start();
   require_once("databaselink.php");
+    if(isset($_GET['page'])){
+        $pages = array("products","cart");
+        if(in_array($_GET['page'],$pages)){
+            $page = $_GET['page'];
+        }
+        else{
+            $page = "products"; 
+    }
+    }else{
+        $page = 'products';
+    }
     echo $_SESSION["userid"];
        
         $page = $_GET['page'];
-        $id = $_GET['id'];
+       $id = $_GET['id'];
         //echo $page;
         echo $id;
+        echo $row['id'];
         
   
 ?>
@@ -22,15 +34,38 @@
         <h1>Cart</h1>
         <div>
 <?php
-
-$cart = array();
-$cart= $_SESSION['cart'];
-    $sql = "SELCT *FROM 'product' WHERE id IN (";$sql .= implode(",", $_SESSION['cart']) . ")";
-echo $cart;
-echo $sql;
+    if(isset($_SESSION['cart'])){
+    
+    $sql ="SELECT * FROM product WHERE id IN ("; 
+    foreach($_SESSION['cart']) as $id => $value){
+            $sql .= $id. ",";
+    }
+    $sql = substr($sql,0,-1) . ") ORDER BY id ASC";
+    $query = mysql_query($sql);
+    while($row = mysql_fetch_array($query)){
 ?>
-
-
+    <p><?php echo $row['name']; ?><?php echo $_SESSION['cart'][$row['id']]['quantity']; ?></p>
+    <a href="index.php?page=cart">Go to cart</a>
+<?php
+             }
+                                       }else{
+    echo "<p>Your cart is empty.<br/> Please add some products</p>";
+}
+?>
+<!--< ?php 
+    echo $_GET['id'] . "<br/>";
+    echo $_SESSION['cart'] . "<br/>";
+    require('databaselink.php');
+    $sql = "SELECT * FROM `product` WHERE id='{$_GET['id']}'";
+    echo $sql . "<br/>";
+    $queryc = mysql_query($sql);
+   while($row = mysql_fetch_assoc($queryc)){
+    
+        echo $row['id'];
+        echo $row['description'];
+        echo $row['price'];
+   }
+?>-->
         </div>
         <a href="logout.php">logout</a>
     </body>
